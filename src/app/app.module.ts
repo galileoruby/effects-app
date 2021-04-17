9ba19feac11c99b3b2835eda8d0c6278122e967b
Galileo Galván <galileo.galvan@interfactura.com>
2021-04-17 14:23:48 -0500

    app sin efectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
+//rutas
 import { AppRoutingModule } from './app-routing.module';
+
+
+//_modulos personalizados
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AppRouterModule,
+    SharedModule,
+    UsuariosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,14 @@
+export class Usuario {
+    // public id: number = 0;
+    // public first_name: string;
+    // public last_name: string;
+    // public last_name: string;
+
+    constructor(
+        public id: number,
+        public first_name: string,
+        public last_name: string,
+        public avatar: string
+    ) { }
+
+}
